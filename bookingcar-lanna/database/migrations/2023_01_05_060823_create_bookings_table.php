a9e3db15d5688f9ee63299b7bda37f98bd5d745e
Jirasak07 <bee.110243@gmail.com>
2023-01-05 13:18:11 +0700

    migrate database

---
+++ bookingcar-lanna/database/migrations/2023_01_05_060823_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->date('booking_start');
+            $table->date('booking_end');
+            $table->string('user');
+            $table->string('driver_book');
+            $table->integer('type_car');
+            $table->integer('booking_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
